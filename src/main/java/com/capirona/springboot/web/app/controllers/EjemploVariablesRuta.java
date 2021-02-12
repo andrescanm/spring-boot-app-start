c8d50c0c29935a32ba91521ddcbd7b96ed207d39
Andrés Cañón M <andres.co@hotmail.com>
2021-02-11 22:32:53 -0500

    Anotacion @PathVariable - Pasar varios parametros GET en la URL.

--- src/main/java/com/capirona/springboot/web/app/controllers/EjemploVariablesRuta.java
+++ src/main/java/com/capirona/springboot/web/app/controllers/EjemploVariablesRuta.java
@@ -10,10 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/variables")
 public class EjemploVariablesRuta {
 	
+	@GetMapping({"/", "/index"})
+	public String index(Model model) {
+		model.addAttribute("titulo", "Spring multiples variables @PathVariable.");
+		return "variables/index";
+	}
+	
 	@GetMapping("/string/{texto}")
 	public String variables(@PathVariable String texto, Model model) {
 		model.addAttribute("titulo", "Spring @PathVariable");
 		model.addAttribute("respuesta", "El valor enviado en el path es: " + texto);
+		return "variables/ver";
+	}
+
+	@GetMapping("/string/{texto}/{numero}")
+	public String variables(@PathVariable String texto, @PathVariable Integer numero, Model model) {
+		model.addAttribute("titulo", "Spring @PathVariable");
+		model.addAttribute("respuesta", "El valor enviado en el path es: " + texto + 
+				". Y el número es: " + numero + ".");
 		return "variables/ver";
 	}
 }
